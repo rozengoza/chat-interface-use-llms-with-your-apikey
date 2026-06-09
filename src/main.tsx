3eb43ad5bff4bf36ad20c8c883175bcdcfce35a4
rozengoza <rozengoza22@gmail.com>
2026-06-09 13:22:35 +0545

    fix: vercel analytics react

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Root from './Root'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 // Apply saved theme before first paint to avoid flash
